6e8512dc7a6da7ba535897d5b7e67e6897768e88
Atthaphon Poosadet <53036250+Atthaphon-p@users.noreply.github.com>
2024-04-23 10:42:12 +0700

    Data _cleansing.py

--- Data _cleansing.py
+++ Data _cleansing.py
@@ -1,3 +1,8 @@
 import numpy as np
 import pandas as pd
-dataset = pd.read_csv("netflix-dataset-for-analysis/netflix.csv")
+dataset = pd.read_csv("netflix.csv") #import data set
+print(dataset.head().columns) #check header col
+mis_values = dataset.isnull().sum() #check null values
+print(mis_values)
+
+
